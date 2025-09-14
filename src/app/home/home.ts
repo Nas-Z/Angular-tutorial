d98671a5b84763030f8003cb6ebdd06e4bb5bed3
NAZ <112337066+Nas-Z@users.noreply.github.com>
2025-09-14 11:04:41 +0300

    first commit

---
+++ src/app/home/home.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housinglocation';
+import {Housing} from '../housing';
+
+@Component({
+  selector: 'app-home',
+  imports: [HousingLocation],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      @for (housingLocation of housingLocationList; track $index) {
+        <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+        }
+    </section>
+  `,
+  styleUrls: ['../home/home.css']
+})
+export class Home {
+
+  housingLocationList: HousingLocationInfo[] = [];
+
+  housinService: Housing = inject(Housing);
+
+  constructor() {
+    this.housingLocationList = this.housinService.getAllHousingLocations();
+  }
+    
+}
+
