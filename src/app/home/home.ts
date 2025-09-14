fa1e604e9af46efb07129c20e5d75a660f8e0ef5
NAZ <112337066+Nas-Z@users.noreply.github.com>
2025-09-14 11:14:48 +0300

    add the search feature

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -9,12 +9,12 @@ import {Housing} from '../housing';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      @for (housingLocation of housingLocationList; track $index) {
+      @for (housingLocation of filteredLocationList; track $index) {
         <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
         }
     </section>
@@ -24,11 +24,24 @@ import {Housing} from '../housing';
 export class Home {
 
   housingLocationList: HousingLocationInfo[] = [];
+  filteredLocationList: HousingLocationInfo[] = [];
 
   housinService: Housing = inject(Housing);
 
   constructor() {
     this.housingLocationList = this.housinService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text){
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
     
 }
